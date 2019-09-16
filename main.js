81e0373a81ac9e1575731d880601bed2c56fa5a5
Ace <50377458+AceBanares@users.noreply.github.com>
2019-09-16 03:43:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,112 @@
+// set game choices as an array with index ranging from 0 to 2
+const gameChoices = ["ROCK", "PAPER", "SCISSORS"];
+// record game results
+const resultTable = [];
+// record game stats
+const gameStats = { gameWins: 0, gameTies: 0, gameLoses: 0 };
+
+// set variables to hold appropriate DOM elements
+const btnPlayerChoice = document.querySelector("#sendChoice");
+const txtPlayerChoice = document.querySelector("#playerChoice");
+const txtCompChoice = document.querySelector("#compChoice");
+const txtGameResult = document.querySelector("#gameResult");
+const txtGameWins = document.querySelector("#gameWins");
+const txtGameTies = document.querySelector("#gameTies");
+const txtGameLoses = document.querySelector("#gameLoses");
+const txtRepeatValue = document.querySelector("#repeatValue");
+
+function getRandom(max = 1, min = 0) {
+  // get a random number from min (default: 0) to max (default: 1)
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getPlayerChoice() {
+  // get the radio button that is currently chosen
+  return document.querySelector(".form-check-input:checked").value;
+}
+
+function getCompChoice() {
+  // use index from 0 to 2 to get a random item from gameChoice
+  return gameChoices[getRandom(2)];
+}
+
+function displayUpdate(
+  info = { playerChoice: "", compChoice: "", gameResult: "" }
+) {
+  // deconstruct the array info (default: empty)
+  const { playerChoice, compChoice, gameResult } = info;
+  // deconstruct the array gameStats
+  const { gameWins, gameTies, gameLoses } = gameStats;
+  // display the choice of both player and computer
+  txtPlayerChoice.textContent = `Your choice: ${playerChoice}`;
+  txtCompChoice.textContent = `Computer's choice: ${compChoice}`;
+  // display the result of the game
+  txtGameResult.textContent = gameResult;
+  txtGameWins.textContent = `Wins: ${gameWins}`;
+  txtGameTies.textContent = `Ties: ${gameTies}`;
+  txtGameLoses.textContent = `Loses: ${gameLoses}`;
+}
+
+function getWinner() {
+  // use the functions above to get both choices
+  const playerChoice = getPlayerChoice();
+  const compChoice = getCompChoice();
+  // get resultIndex by subtracting the indices of the choices
+  let resultIndex =
+    gameChoices.indexOf(playerChoice) - gameChoices.indexOf(compChoice);
+
+  // if resultIndex is -2 then it is a win
+  if (resultIndex < -1) resultIndex = 1;
+  // if resultIndex is 2 then it is a lost
+  if (resultIndex > 1) resultIndex = -1;
+
+  // check RESULTS MATRIX below
+  let gameResult = "";
+  switch (resultIndex) {
+    case -1:
+      gameResult = "You LOST!";
+      gameStats.gameLoses++;
+      break;
+    case 0:
+      gameResult = "It's a TIE!";
+      gameStats.gameTies++;
+      break;
+    case 1:
+      gameResult = "You WON!";
+      gameStats.gameWins++;
+      break;
+    default:
+      console.log("Result is not defined!");
+  }
+
+  // set an object as a parameter to update the display
+  const info = { playerChoice, compChoice, gameResult };
+
+  resultTable.push(info);
+  displayUpdate(info);
+}
+
+// RESULTS MATRIX
+// ---------------------------------------------
+//            rock        paper       scissors
+// value      0	          1           2
+// ---------------------------------------------
+// 		        rock	      paper	      scissors
+// rock		    0 tie	      -1 lose 	  -2 win
+// paper		  1 win	      0 tie	      -1 lose
+// scissors	  2 lose	    1 win	      0 tie
+
+// wait for player to click the button to declare the winner
+btnPlayerChoice.addEventListener("click", () => {
+  const repeatValue =
+    isNaN(txtRepeatValue.value) || txtRepeatValue.value == ""
+      ? 1
+      : txtRepeatValue.value;
+  for (let repeat = 0; repeat < repeatValue; repeat++) {
+    getWinner();
+  }
+  // txtRepeatValue.value = "";
+});
+
+// initialize the display
+displayUpdate();
